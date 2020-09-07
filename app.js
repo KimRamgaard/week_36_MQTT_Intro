e7ee7ee7f23f2f5282183da6995410e808562fac
Kim Ramgaard <kim.ramgaard@gmail.com>
2020-09-07 11:24:55 +0200

    Debug

--- app.js
+++ app.js
@@ -11,19 +11,8 @@ let MQTTclient  = mqtt.connect(
     password: process.env.MQTT_PASSWORD
   })
 
-MQTTclient.on('connect', function (client, userdata, flags, rc) {
-  console.log(client);
-  console.log("connected to MQTT Client");
-})
-
-
-
 app.get('/', (req, res) => {
   console.log("is connected to broker: " + MQTTclient.connected) 
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
 })
 
 MQTTclient.subscribe('#');
@@ -34,19 +23,9 @@ MQTTclient.on('message', function (topic, message) {
   MQTTclient.end()
 })
 
-
-/*
-MQTTclient.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString())
-  // client.end()
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
-*/
-
-
-
-
-//MQTTclient.subscribe('#');
 
 /*
 app.get('/', (req, res) => {
