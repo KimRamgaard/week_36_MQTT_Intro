f4a7a7e939a4e2085084ae720badb4891571cc7c
Kim Ramgaard <kim.ramgaard@gmail.com>
2020-09-07 09:42:51 +0200

    Added MQTT

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8888
+const port = process.env.PORT || 3000
+var mqtt = require('mqtt')
 
-app.get('/', (req, res) => {
-  res.send('Hello World! test')
+let MQTTclient  = mqtt.connect(
+  'mqtt://' + process.env.MQTT_ADDRESS,
+  {
+    port: +process.env.MQTT_PORT,
+    username: process.env.MQTT_USERNAME,
+    password: process.env.MQTT_PASSWORD
+  })
+
+  MQTTclient.on('connect', function () {
+  console.log('Connected to mqtt client');
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+})
+
+MQTTclient.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  // client.end()
+})
+
+
+MQTTclient.subscribe('#');
+
+
+app.get('/', (req, res) => {
+  MQTTclient.publish("kim.kool", "{i:understand, j:son}")
+  res.send('Hello World!')
+})
+
+app.get('/rest', (req, res) => {
+  res.send('very restfull')
 })
 
