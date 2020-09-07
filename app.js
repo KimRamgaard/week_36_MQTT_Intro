5530440c812b49bf2792cffc6f2312ad80f408bb
Kim Ramgaard <kim.ramgaard@gmail.com>
2020-09-07 09:19:46 +0200

    Init commit

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8888
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! test')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
